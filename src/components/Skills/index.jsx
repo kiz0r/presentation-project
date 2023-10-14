86268fb96ecd133483225c695014b4b5ef56bf68
vaferiiaa <lerafesenco@gmail.com>
2023-10-14 16:42:19 +0200

    Finish with Skills

--- src/components/Skills/index.jsx
+++ src/components/Skills/index.jsx
@@ -1,4 +1,4 @@
-import { useTranslation } from 'react-i18next'
+import { useTranslation, Trans } from 'react-i18next'
 import { SKILLS } from '../../constants'
 
 import styles from './Skills.module.scss'
@@ -7,16 +7,21 @@ const Skills = () => {
   const { t } = useTranslation()
   return (
     <section id='skills' className={styles.skillSection}>
-      {SKILLS.map(fieldSkills => (
-        <div key={fieldSkills.field} className={styles.skillBox}>
-          <p className={styles.skillHeadline}>{t(fieldSkills.field)}</p>
-          <ul className={styles.skillsList}>
-            {fieldSkills.skills.map(skill => (
-              <li key={skill}>{t(skill)}</li>
-            ))}
-          </ul>
-        </div>
-      ))}
+      <h2>{t('skills')}</h2>
+      <div className={styles.skillsContainer}>
+        {SKILLS.map(fieldSkills => (
+          <div key={fieldSkills.field} className={styles.skillBox}>
+            <p className={styles.skillHeadline}>{t(fieldSkills.field)}</p>
+            <ul className={styles.skillsList}>
+              {fieldSkills.skills.map(skill => (
+                <li key={skill}>
+                  <Trans components={{ br: <br /> }}>{skill}</Trans>
+                </li>
+              ))}
+            </ul>
+          </div>
+        ))}
+      </div>
     </section>
   )
 }
