a0f2e873404945483ff1f341f2e848165caf0c2b
vaferiiaa <lerafesenco@gmail.com>
2023-10-15 14:59:14 +0200

    add errors to form

--- src/components/FormInputs/index.jsx
+++ src/components/FormInputs/index.jsx
@@ -1,8 +1,10 @@
 import { Field, useField } from 'formik'
+import { useTranslation } from 'react-i18next'
 
 import styles from './FormInput.module.scss'
 
 const FormInput = ({ name, label, ...restProps }) => {
+  const { t } = useTranslation()
   return (
     <Field name={name}>
       {({ field, form: { errors, touched }, meta }) => {
@@ -10,7 +12,7 @@ const FormInput = ({ name, label, ...restProps }) => {
           <label className={styles.formLabel}>
             {label && <span>{label}</span>}
             <input {...restProps} {...field} className={styles.formInput} />
-            {meta.error && meta.touched && <div>{meta.error}</div>}
+            {meta.error && meta.touched && <div>{t(meta.error)}</div>}
           </label>
         )
       }}
@@ -20,24 +22,26 @@ const FormInput = ({ name, label, ...restProps }) => {
 
 const FormTextArea = ({ label, ...restProps }) => {
   const [field, meta] = useField(restProps)
+  const { t } = useTranslation()
 
   return (
     <label className={styles.formLabel}>
       {label && <span>{label}</span>}
       <textarea {...field} {...restProps} className={styles.formInput} />
-      {meta.error && meta.touched && <div>{meta.error}</div>}
+      {meta.error && meta.touched && <div>{t(meta.error)}</div>}
     </label>
   )
 }
 
 const FormSelect = ({ label, ...restProps }) => {
   const [field, meta] = useField(restProps)
+  const { t } = useTranslation()
 
   return (
     <label className={styles.formLabel}>
       {label && <span>{label}</span>}
       <select {...field} {...restProps} className={styles.formInput} />
-      {meta.error && meta.touched && <div>{meta.error}</div>}
+      {meta.error && meta.touched && <div>{t(meta.error)}</div>}
     </label>
   )
 }
--- src/utils/validation.js
+++ src/utils/validation.js
@@ -1,35 +1,21 @@
 import * as yup from 'yup';
 
-// const NAME_VALIDATION_SCHEMA = yup
-//   .string()
-//   .required()
-//   .min(5, 'Name must be at least 5 characters')
-//   .max(16, 'Name must be at most 16 characters')
-//   .lowercase('Name should contain only lowercase letters')
-//   .required('Please enter your name');
-
-// const EMAIL_VALIDATION_SCHEMA = yup
-//   .string()
-//   .email()
-//   .required('Please enter your email');
-
 const NAME_VALIDATION_SCHEMA = yup
   .string()
   .required()
-  .min(5, 'nameMinError')
+  .min(4, 'nameMinError')
   .max(16, 'nameMaxError')
-  .lowercase('nameLowercaseError')
-  .required('nameRequiredError');
+  .required('requiredError');
 
 const PHONE_VALIDATION_SCHEMA = yup
   .string()
   .matches(/^\+(380|420)\d{9}$/, 'phoneMatchesError')
-  .required('phoneRequiredError');
+  .required('requiredError');
 
 const EMAIL_VALIDATION_SCHEMA = yup
   .string()
-  .email()
-  .required('emailRequiredError');
+  .email('emailValidError')
+  .required('requiredError');
 
 export const CONTACT_FORM_VALIDATION_SCHEMA = yup.object({
   userName: NAME_VALIDATION_SCHEMA,
